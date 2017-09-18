06986e47d731f31aa7a703d6c89abae0351df1f7
Daniel Wiese <wiesed@gmail.com>
2017-09-18 13:01:34 +0200

    svm fur linux und windows

---
+++ lib/install.bat.sh
@@ -0,0 +1,3 @@
+call mvn install:install-file -Dfile=svmLight-mac-8.4.jar -DgroupId=svm.light -DartifactId=svmLight-mac -Dversion=8.4 -Dpackaging=jar
+call mvn install:install-file -Dfile=svmLight-linux-8.4.jar -DgroupId=svm.light -DartifactId=svmLight-linux -Dversion=8.4 -Dpackaging=jar
+call mvn install:install-file -Dfile=svmLight-windows-8.4.jar -DgroupId=svm.light -DartifactId=svmLight-windows -Dversion=8.4 -Dpackaging=jar
